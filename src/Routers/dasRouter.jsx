4d7879e9335b98f04a0f5e32b37a4b779eaa845d
Abdul Ahad <99830052+AbdulAhadilyas@users.noreply.github.com>
2022-11-16 20:23:06 +0500

    add component

--- src/Routers/dasRouter.jsx
+++ src/Routers/dasRouter.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import Dashbord from '../pages/dashboard/Dashbord.jsx';
 import { Route, Routes,  } from 'react-router-dom';
-import StudentForm from '../component/student from/studentform.jsx';
-import CreatClass from '../component/create class/CreatClass.jsx';
+import StudentForm from '../component/Add Student Form/StudentForm';
+import CreateClass from '../component/Create Class/CreateClass';
+import StdData from '../component/get Student data/StdData';
+import Home from '../component/home/Home';
+
+
 
 const DasRouters = () => {
     return (
         <Routes>
+             <Route path="/" element={<Home/>} />
              <Route path="/student" element={<StudentForm/>} />
-             <Route path="/createclass" element={<CreatClass/>} />
+             <Route path="/student/data" element={<StdData/>} />
+             <Route path="/class" element={<CreateClass/>} />
         </Routes>
     );
 }
