fecbf9c4f2ca2a5b5afe7a5f3e140aff1e2411c4
Abdul Ahad <99830052+AbdulAhadilyas@users.noreply.github.com>
2022-11-17 22:23:40 +0500

    add some code

--- src/component/dashboard/DashBoard.jsx
+++ src/component/dashboard/DashBoard.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import DasRouters from '../../Routers/DasRouter'
 import StudentForm from '../Add Student Form/StudentForm'
-import { Link } from 'react-router-dom'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 import "./DashBoard.css"
 
 export default function DashBoard() {
+  const location = useLocation();
+
+  //destructuring pathname from location
+  const { pathname } = location;
+
+  //Javascript split method to get the name of the path in array
+  const splitLocation = pathname;
   return (
     <div>
       <nav>
@@ -30,7 +37,7 @@ export default function DashBoard() {
         </div>
       </nav>
       <div className="flex">
-        <aside>
+        {/* <aside>
           <div className="box-side">
             <div className="list">
               <ul>
@@ -43,7 +50,7 @@ export default function DashBoard() {
                   </Link>
                 </li>
                 <li>
-                <Link to={"/class"}>
+                <Link to={"/"}>
                   <div className="hover-elemnt">
                     <i className="fa-solid fa-chalkboard-user"></i>
                     <div className="list-iteam">Create Class</div>
@@ -77,7 +84,44 @@ export default function DashBoard() {
               </ul>
             </div>
           </div>
-        </aside>
+        </aside> */}
+        <div className="app">
+          <div className="menu-toggle is-active">
+            <div className="hamburger">
+              <span></span>
+            </div>
+          </div>
+
+          <aside className="sidebar ">
+            <div className="over">
+              <div className="head-box">
+                <h3>Dashboard</h3>
+              </div>
+              <nav className="menu ">
+                <Link to={"/"} className={splitLocation === "/" ? "menu-item is-active" : "menu-item"}>
+                  Home
+                </Link>
+                <Link to={"/student"} className={splitLocation === "/student" ? "menu-item is-active" : "menu-item "}>
+                  Add student
+                </Link>
+                <Link to={"/student/data"} className={splitLocation === "/student/data" ? "menu-item is-active" : "menu-item "}>
+                  Get Student Data
+                </Link>
+                <Link to={"/class"} className={splitLocation === "/class" ? "menu-item is-active" : "menu-item "}>
+                  Create Class
+                </Link>
+                <Link className="menu-item">
+                  Contact
+                </Link>
+                <Link className="menu-item">
+                  Contact
+                </Link>
+              </nav>
+            </div>
+          </aside>
+
+        </div>
+
 
         <section>
           <DasRouters />
